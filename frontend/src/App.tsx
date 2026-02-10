cc5d03af8c8293667ae613836207c865852e2f69
Bryan Personal <bryanlohjw97@gmail.com>
2026-02-11 10:48:19 +1100

    feat: header + navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,79 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import freeRoomOpen from './assets/freeRoomsLogo.png'
+import freeRoomClose from './assets/freeroomsDoorClosed.png'
+import {
+	Search,
+	Map,
+	GridViewRounded,
+	DarkMode,
+	FilterAlt,
+	FilterList,
+} from '@mui/icons-material'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [isIconOpen, setIsIconOpen] = useState(true)
+
+	const selectedStyle = 'text-white bg-primary'
+	const unselectedStyle = 'text-primary bg-white'
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
+			<header className="w-full flex items-center justify-between border-b border-gray-400 px-2">
+				<div className="flex items-center">
 					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
+						src={isIconOpen ? freeRoomOpen : freeRoomClose}
+						className="h-12 cursor-pointer"
+						alt="logo"
+						onClick={() => setIsIconOpen(!isIconOpen)}
 					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
+					<h1 className="hidden md:block text-primary text-3xl font-medium">
+						Freerooms
+					</h1>
+				</div>
+				<div className="flex gap-2">
+					<button
+						className={`p-1 border border-primary rounded ${unselectedStyle} active:scale-110`}
+					>
+						<Search />
+					</button>
+					<button
+						className={`p-1 border border-primary rounded ${selectedStyle} active:scale-110`}
+					>
+						<GridViewRounded />
+					</button>
+					<button
+						className={`p-1 border border-primary rounded ${unselectedStyle} active:scale-110`}
+					>
+						<Map />
+					</button>
+					<button
+						className={`p-1 border border-primary rounded ${unselectedStyle} active:scale-110`}
+					>
+						<DarkMode />
+					</button>
+				</div>
+			</header>
+			<nav className="flex flex-wrap px-2 justify-between my-4 gap-2">
+				<button className="order-2 md:order-1 shrink-0 h-12 px-4 w-36 rounded-lg text-primary font-bold flex items-center gap-4 border-2 border-primary active:scale-110">
+					<FilterAlt />
+					<p>Filters</p>
 				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+				<div className="order-1 md:order-2 w-full max-w-200 flex gap-4 border border-slate-300 text-gray-700 rounded-lg h-10 items-center p-6 md:w-1/2 focus-within:ring-2">
+					<div className="">
+						<Search />
+					</div>
+					<input
+						className="flex-1 border-none outline-none"
+						type="text"
+						placeholder="Search for a building..."
+					/>
+				</div>
+				<button className="order-3 shrink-0 h-12 px-4 w-36 rounded-lg text-primary font-bold flex items-center gap-4 border-2 border-primary active:scale-110">
+					<FilterList />
+					<p>Sort</p>
+				</button>
+			</nav>
 		</>
 	)
 }

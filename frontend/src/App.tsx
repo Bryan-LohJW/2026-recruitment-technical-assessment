8618c88e6dea20098fd58b9bef4c3b908ad9f01e
Bryan Personal <bryanlohjw97@gmail.com>
2026-02-11 23:16:47 +1100

    feat: responsive styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -77,7 +77,7 @@ function App() {
 					<p>Sort</p>
 				</button>
 			</nav>
-			<div className="m-2 h-full grid grid-cols-1 gap-6">
+			<div className="m-2 h-full grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
 				{buildingData.map((building) => (
 					<Building key={building.name} {...building} />
 				))}
@@ -91,14 +91,24 @@ export default App
 const Building = (data: Building) => {
 	return (
 		<div
-			className={`transition-transform duration-100 shadow active:scale-105 relative flex justify-between items-center h-30 w-full rounded-xl overflow-hidden p-4 text-white font-semibold`}
+			className={`transition-transform duration-100 shadow active:scale-105 hover:shadow-2xl relative flex justify-between items-center md:flex-col-reverse md:items-end h-30 w-full rounded-xl overflow-hidden p-4 text-white font-semibold md:h-52 lg:h-104`}
 		>
-			<p className="">{data.name}</p>
-			<div className="flex items-center gap-4 bg-white text-black px-4 py-2 rounded-3xl text-sm">
+			<p className="md:bg-primary md:w-full md:h-14 md:flex md:items-center md:p-4 md:rounded-xl">
+				{data.name}
+			</p>
+			<div className="flex items-center gap-4 bg-white text-black text-xs px-4 py-3 rounded-3xl">
 				<div className="bg-green-600 h-3 w-3 rounded-full"></div>
-				<p>
-					{data.rooms_available}/{data.rooms_available}
-				</p>
+				<div className="flex">
+					<p>{data.rooms_total}</p>
+					<p className="md:hidden">/{data.rooms_total}</p>
+					<p
+						className="hidden md:block"
+						style={{ whiteSpace: 'pre' }}
+					>
+						{' '}
+						rooms available
+					</p>
+				</div>
 			</div>
 			<div
 				className="absolute top-0 left-0 w-full h-full bg-cover bg-center brightness-60 -z-10"
--- frontend/src/hooks/useStaticData.tsx
+++ frontend/src/hooks/useStaticData.tsx
@@ -1,6 +1,6 @@
 export type Building = {
 	name: string
-	rooms_available: number
+	rooms_total: number
 	building_picture: string
 }
 
@@ -8,52 +8,52 @@ const useStaticData = (): Building[] => {
 	return [
 		{
 			name: 'AGSM',
-			rooms_available: 9,
+			rooms_total: 9,
 			building_picture: '/agsm.webp',
 		},
 		{
 			name: 'Ainsworth Building',
-			rooms_available: 16,
+			rooms_total: 16,
 			building_picture: '/ainsworth.webp',
 		},
 		{
 			name: 'Anita B Lawrence Centre',
-			rooms_available: 44,
+			rooms_total: 44,
 			building_picture: '/anitab.webp',
 		},
 		{
 			name: 'Biological Sciences',
-			rooms_available: 6,
+			rooms_total: 6,
 			building_picture: '/biologicalScience.webp',
 		},
 		{
 			name: 'Biological Science (West)',
-			rooms_available: 8,
+			rooms_total: 8,
 			building_picture: '/biologicalScienceWest.webp',
 		},
 		{
 			name: 'Blockhouse',
-			rooms_available: 42,
+			rooms_total: 42,
 			building_picture: '/blockhouse.webp',
 		},
 		{
 			name: 'Business School',
-			rooms_available: 18,
+			rooms_total: 18,
 			building_picture: '/businessSchool.webp',
 		},
 		{
 			name: 'Civil Engineering Building',
-			rooms_available: 8,
+			rooms_total: 8,
 			building_picture: '/civilBuilding.webp',
 		},
 		{
 			name: 'Colombo Building',
-			rooms_available: 5,
+			rooms_total: 5,
 			building_picture: '/colombo.webp',
 		},
 		{
 			name: 'Computer Science & Eng (K17)',
-			rooms_available: 7,
+			rooms_total: 7,
 			building_picture: '/cseBuilding.webp',
 		},
 	]
